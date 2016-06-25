8980fd592103febf425ad55e8b32110c3edfc09c
liucl <aabbcclcl@hotmail.com>
2016-06-25 23:25:11 +0800

    修改名字

--- vmaig_auth/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from vmaig_auth.views import UserControl
-
-
-urlpatterns = [
-        url(r'^usercontrol/(?P<slug>\w+)$', UserControl.as_view()),
-]
--- vmaig_comments/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from vmaig_comments.views import CommentControl
-
-
-urlpatterns = [
-        url(r'^comment/(?P<slug>\w+)$', CommentControl.as_view()),
-]
